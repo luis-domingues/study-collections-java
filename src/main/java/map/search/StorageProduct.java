a257a0d4b0b3272613884c46393ba94c92dbd4b4
Luís Paulo Domingues <157630664+luis-domingues@users.noreply.github.com>
2024-09-12 10:51:53 -0300

    feat: implementar gerenciamento de estoque com operações de adição, cálculo de valor total e busca do produto mais caro

--- src/main/java/map/search/StorageProduct.java
+++ src/main/java/map/search/StorageProduct.java
@@ -1,2 +1,57 @@
-package main.java.map.search;public class StorageProduct {
+package main.java.map.search;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StorageProduct {
+    private final Map<Long, Product> storageProductMap;
+
+    public StorageProduct () {
+        this.storageProductMap = new HashMap<> ();
+    }
+
+    public void addProduct (long cod, String name, int quantity, double price) {
+        storageProductMap.put (cod, new Product (name, price, quantity));
+    }
+
+    public void displayProduct () {
+        System.out.println (storageProductMap);
+    }
+
+    public double calcTotalStorageValue () {
+        double totalStorageValue = 0d;
+        if (!storageProductMap.isEmpty ()) {
+            for (Product p : storageProductMap.values ()) {
+                totalStorageValue += p.getQuantity () + p.getPrice ();
+            }
+        }
+
+        return totalStorageValue;
+    }
+
+    public Product getMoreExpensiveProduct () {
+        Product mostExpensiveProduct = null;
+        double highestPrice = Double.MIN_VALUE;
+        for (Product p : storageProductMap.values ()) {
+            if (p.getPrice () > highestPrice) {
+                mostExpensiveProduct = p;
+            }
+        }
+
+        return mostExpensiveProduct;
+    }
+
+    public static void main (String[] args) {
+        StorageProduct storageProduct = new StorageProduct ();
+
+        storageProduct.addProduct (1L, "Product 01", 50, 10.0);
+        storageProduct.addProduct (2L, "Product 02", 100, 5.0);
+        storageProduct.addProduct (3L, "Product 03", 15, 45.0);
+        storageProduct.addProduct (4L, "Product 04", 75, 25.0);
+
+        storageProduct.getMoreExpensiveProduct ();
+
+        System.out.println ("Valor total em estoque: R$" + storageProduct.calcTotalStorageValue ());
+        System.out.println ("Produto mais caro em estoque: " + storageProduct.getMoreExpensiveProduct ());
+    }
 }
